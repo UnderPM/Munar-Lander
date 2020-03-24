195909916a38156ccc686a2e5b2e1fda7f109c8e
UnderPM <UnderPM@users.noreply.github.com>
2020-03-24 00:28:22 -0300

    Added custom gravity script

---
+++ Munar-Lander/Assets/Scripts/CustomGravity.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class CustomGravity : MonoBehaviour
+{
+    [SerializeField] float gravityScale = 1.0f;
+
+    float globalGravity = -9.81f;
+
+    Rigidbody rigidBody;
+
+    private void OnEnable()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        rigidBody.useGravity = false;
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 gravity = globalGravity * gravityScale * Vector3.up;
+        rigidBody.AddForce(gravity, ForceMode.Acceleration);
+    }
+}
